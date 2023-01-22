7c5c05f8ebcbc68d7e5c5e8cb07759fc13aa6353
patrick brisbin <pbrisbin@gmail.com>
2023-01-22 09:46:56 -0500

    Layout

--- src/Bluebook/CSS.hs
+++ src/Bluebook/CSS.hs
@@ -21,6 +21,16 @@ header {
     border-bottom: 1px solid #A00000;
 }
 
+nav ul {
+    margin-left: 0;
+    padding-left: 0;
+}
+
+nav li {
+    display: inline-block;
+    padding-right: 1em;
+}
+
 h1, h2, h3, h4, h5, h6, strong {
     color: #A00000;
 }
@@ -29,5 +39,13 @@ em {
     color: #006000;
     font-style: normal;
     font-weight: bold;
+}
+
+footer {
+    color: #808080;
+    font-size: 90%;
+    padding-bottom: 1em;
+    padding-top: 1em;
+    text-align: center;
 }
 |]
--- src/Bluebook/HTML.hs
+++ src/Bluebook/HTML.hs
@@ -5,13 +5,30 @@ module Bluebook.HTML
 import Bluebook.Prelude hiding (head)
 
 import qualified Bluebook.CSS as CSS
-import Text.Blaze.Html5
-import Text.Blaze.Html5.Attributes (charset)
+import Bluebook.ManPage.Section
+import qualified Text.Blaze.Html5 as Html
+import Text.Blaze.Html5 (Html, docTypeHtml, toHtml, toValue, (!))
+import qualified Text.Blaze.Html5.Attributes as Html (charset, href)
 
 layout :: Text -> Html -> Html
-layout t inner = docTypeHtml $ do
-    head $ do
-        meta ! charset "UTF-8"
-        title $ toHtml t
-        style $ toHtml CSS.styles
-    body inner
+layout title body = docTypeHtml $ do
+    Html.head $ do
+        Html.meta ! Html.charset "UTF-8"
+        Html.title $ toHtml title
+        Html.style $ toHtml CSS.styles
+    Html.body $ do
+        Html.nav $ do
+            Html.ul $ do
+                Html.li $ (Html.a ! Html.href "/") "Home"
+                for_ [minBound .. maxBound] $ \section -> do
+                    let path = sectionPath section
+                        link = toValue $ "/" <> path
+                    Html.li $ (Html.a ! Html.href link) $ toHtml path
+
+        body
+
+        Html.footer $ do
+            "Built with "
+            (Html.a ! Html.href "https://github.com/pbrisbin/bluebook")
+                "Bluebook"
+            " © 2023 Patrick Brisbin"
