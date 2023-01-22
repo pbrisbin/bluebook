e195baa02a93b7ffd55e35c85bc56debec5ed430
patrick brisbin <pbrisbin@gmail.com>
2023-01-22 09:34:07 -0500

    Settings.hs

--- src/Bluebook/Settings.hs
+++ src/Bluebook/Settings.hs
@@ -5,6 +5,9 @@ module Bluebook.Settings
 
 import Bluebook.Prelude
 
+import qualified Env
+import qualified System.Environment.XDG.BaseDir as XDG
+
 data Settings = Settings
     { settingsPort :: Int
     , settingsManPath :: [FilePath]
@@ -12,13 +15,18 @@ data Settings = Settings
 
 loadSettings :: MonadIO m => m Settings
 loadSettings = do
-    port <- lookupEnv "PORT"
+    manPath <- getDefaultManPath
 
-    pure Settings
-        { settingsPort = fromMaybe 3000 $ readMaybe =<< port
-        , settingsManPath =
-            [ "/home/patrick/.local/share/man"
-            , "/usr/local/share/man"
-            , "/usr/share/man"
-            ]
-        }
+    liftIO
+        $ Env.parse (Env.header "Build and serve local man-pages as HTML")
+        $ Settings
+        <$> Env.var Env.auto "PORT" (Env.def 3000)
+        <*> Env.var
+                (Env.splitOn ':' <=< Env.nonempty)
+                "MANPATH"
+                (Env.def manPath)
+
+getDefaultManPath :: MonadIO m => m [FilePath]
+getDefaultManPath = liftIO $ do
+    manDataDir <- XDG.getUserDataDir "man"
+    pure [manDataDir, "/usr/local/share/man", "/usr/share/man"]
